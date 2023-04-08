5327fce49030d77da3d7bf7711a2cc7ef1b547ef
murataydemir9 <119438420+murataydemir9@users.noreply.github.com>
2023-04-08 17:04:29 +0300

    Collectible - Audio
    add collectible and audio

--- My project/Assets/Scripts/MiniGameMovement.cs
+++ My project/Assets/Scripts/MiniGameMovement.cs
@@ -9,7 +9,9 @@ public class MiniGameMovement : MonoBehaviour
     
     public float MoveSpeed = 5f;
     public Rigidbody2D rb;
-    
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
     private Vector2 movement;
 
     
@@ -18,6 +20,12 @@ public class MiniGameMovement : MonoBehaviour
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Horizontal", movement.x);
+        
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        spriteRenderer.flipX = movement.x < 0.01 ? true : false;
     }
 
     private void FixedUpdate()
