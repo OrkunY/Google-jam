b31ee3d7efe8c18f813e82faf2bf5b13ccaa7310
Sabriye Nur <79248086+sabisay@users.noreply.github.com>
2023-04-09 15:38:23 +0300

    başlangıç_UI_düzenlemerli

--- My project/Assets/Scripts/NAME.cs
+++ My project/Assets/Scripts/NAME.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ButtonScript : MonoBehaviour
+public class NAME : MonoBehaviour
 {
-    
+    public InputField NameInput;
     public void Button()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
+
 }
- 
